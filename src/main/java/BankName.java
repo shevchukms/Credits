c9e9db3421b5164c9692ff1dbfcf5cfce9a43bb1
Mykola Shevchuk <30670820+shevchukms@users.noreply.github.com>
2017-08-30 21:06:05 +0300

    commit

---
+++ src/main/java/BankName.java
@@ -0,0 +1,5 @@
+
+public enum BankName {
+
+    PRIVATBANK, OSHCHADBANK,  KREDOBANK;
+}
--- src/main/java/Banks.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Mykola on 30.08.2017.
- */
-public enum Banks {
-
-    PRIVATBANK, OSHCHADBANK,  KREDOBANK;
-}
